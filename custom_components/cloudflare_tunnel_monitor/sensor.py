f652545ea86299ded00bfa1bdc680290521c0359
Jorg Wolski <62062871+jorgwolski@users.noreply.github.com>
2024-01-15 01:03:45 +0100

    Add query parameter to sensor.py
    Adding query parameter is_deleted=false to remove deleted tunnels from API response

--- custom_components/cloudflare_tunnel_monitor/sensor.py
+++ custom_components/cloudflare_tunnel_monitor/sensor.py
@@ -50,7 +50,7 @@ async def fetch_tunnels(email, api_key, account_id, hass):
         'X-Auth-Key': api_key,
         'Content-Type': 'application/json',
     }
-    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/tunnels"
+    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/tunnels?is_deleted=false"
     
     async with aiohttp.ClientSession() as session:
         try:
